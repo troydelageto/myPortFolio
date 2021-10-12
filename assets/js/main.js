ec3a2543d58c66435cda6fe8312536d30c2bcaf9
troydelageto <leroyroy033@gmail.com>
2021-10-12 16:40:24 +0100

    first commit for portofolio

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+//  ====== Menu Show ==========//
+const showMenu=(toggleId,navId)=>{
+    const toggle=document.getElementById(toggleId)
+    const nav=document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav__toggle','nav-menu')
+
+// ======== Active nad remove Menu ========//
+const navLink=document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    // Active Link
+    navLink.forEach(n=>n.classList.remove('active'))
+    this.classList.add('active')
+
+    // remove menu mobile
+    const navMenu =document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+
+// ====== Scroll and Reveal Animation =======//
+
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000,
+    reset:true
+})
+
+// ====== Scroll Home ======= //
+sr.reveal('.home__title',{})
+sr.reveal('.button',{delay:200})
+sr.reveal('.home__img',{delay:400})
+sr.reveal('.home__social-icon',{interval:200})
+
+
+// === Scroll About ======= //
+
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay:200})
+sr.reveal('.about__text',{delay:400})
+
+// =========== Scroll Skills =======/
+
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay:200})
+sr.reveal('.skills__data',{interval:200})
+sr.reveal('.skills__img',{delay:400})
+
+// ====Scroll Work  =====/
+sr.reveal('.work__img',{interval:200})
+
+// ====Scroll Contact =====/
+sr.reveal('.contact__input',{interval:200})
